b995c4539ab9fad691749dec482b9eb415105d01
Gustavo H T Cardoso <ghtcardoso@icloud.com>
2022-02-02 16:11:26 -0300

    first commit

--- src/Loan.jl
+++ src/Loan.jl
@@ -1,5 +1,13 @@
 module Loan
 
-# Write your package code here.
+export present_value
+"""
+    present_value(amount,rate,dueDate,presentDate=today())
+
+Return the present value of the `amount` due to `dueDate` discounted by the `rate`.
+
+"""
+function present_value(amount,rate,dueDate,presentDate=today())
+end
 
 end
