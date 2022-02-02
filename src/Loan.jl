0ae97eb73fabf9f8ba190588547618f6a6299db4
Gustavo H T Cardoso <ghtcardoso@icloud.com>
2022-02-02 18:49:19 -0300

    pmt function

--- src/Loan.jl
+++ src/Loan.jl
@@ -11,7 +11,6 @@ const DAYS_OF_PERIOD=Dict("day"=>1,"month"=>30,"year"=>365.25)
     present_value(amount,rate,dueDate,presentDate;period="month")
 
 Return the value at presentDate of value of the `amount` due to `dueDate` discounted by the `rate`.
-
 """
 
 function present_value(amount,rate,dueDate,presentDate;period="month")
@@ -23,10 +22,21 @@ end
     present_value(amount,rate,dueDate;period="month")
 
 Return the present value of the `amount` due to `dueDate` discounted by the `rate`.
-
 """
 
 present_value(amount,rate,dueDate;period="month")= present_value(amount,rate,dueDate,Dates.today(),period=period)
+
+
+
+factor_price(rate,qtPeriods,qtPeriodsFirst) = 1/(1+rate)^(qtPeriods+qtPeriodsFirst-1)
+factor_price(rate,qtPeriodsFirst) = (qtPeriods)->factor_price(rate,qtPeriods,qtPeriodsFirst)
+
+function total_payment(amount::Number,rate::Number,initialDate::Dates.Date,dueDates::AbstractVector{Dates.Date};period="month")
+    qtsPeriods = dueDates .|> dueDate-> (dueDate-initialDate).value/DAYS_OF_PERIOD[period]
+    qtPeriodsFirst = minimum(qtDays)
+    fp = factor_price(rate,qtPeriodsFirst)
+    amount/sum(map(fp,qtsPeriods))
+end
 
 
 end
