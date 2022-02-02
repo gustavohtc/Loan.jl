55ef30f12752522e4b46526950bfdbe7f6db50c2
Gustavo H T Cardoso <ghtcardoso@icloud.com>
2022-02-02 16:43:27 -0300

    doc

--- src/Loan.jl
+++ src/Loan.jl
@@ -8,9 +8,9 @@ const DAYS_OF_PERIOD=Dict("day"=>1,"month"=>30,"year"=>365.25)
 
 
 """
-    present_value(amount,rate,dueDate,presentDate=Dates.today())
+    present_value(amount,rate,dueDate,presentDate;period="month")
 
-Return the present value of the `amount` due to `dueDate` discounted by the `rate`.
+Return the value at presentDate of value of the `amount` due to `dueDate` discounted by the `rate`.
 
 """
 
@@ -18,6 +18,14 @@ function present_value(amount,rate,dueDate,presentDate;period="month")
     period ∉ keys(DAYS_OF_PERIOD) && error("Period not defined, must be $(join(keys(DAYS_OF_PERIOD),", "," or "))")
     amount*(1+rate)^((presentDate-dueDate).value/DAYS_OF_PERIOD[period])
 end
+
+"""
+    present_value(amount,rate,dueDate;period="month")
+
+Return the present value of the `amount` due to `dueDate` discounted by the `rate`.
+
+"""
+
 present_value(amount,rate,dueDate;period="month")= present_value(amount,rate,dueDate,Dates.today(),period=period)
 
 
