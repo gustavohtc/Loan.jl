8424639aca41d43c31d1cde21d7e701f54320111
Gustavo H T Cardoso <ghtcardoso@icloud.com>
2022-02-02 16:26:40 -0300

    add present_value function

--- src/Loan.jl
+++ src/Loan.jl
@@ -1,13 +1,24 @@
 module Loan
 
+import Dates
+
 export present_value
+
+const DAYS_OF_PERIOD=Dict("day"=>1,"month"=>30,"year"=>365.25)
+
+
 """
-    present_value(amount,rate,dueDate,presentDate=today())
+    present_value(amount,rate,dueDate,presentDate=Dates.today())
 
 Return the present value of the `amount` due to `dueDate` discounted by the `rate`.
 
 """
-function present_value(amount,rate,dueDate,presentDate=today())
+
+function present_value(amount,rate,dueDate,presentDate;period="month")
+    period ∉ keys(DAYS_OF_PERIOD) && error("Period not defined, must be $(join(keys(DAYS_OF_PERIOD),", "," or "))")
+    amount*(1+rate)^((presentDate-dueDate).value/DAYS_OF_PERIOD[period])
 end
+present_value(amount,rate,dueDate;period="month")= present_value(amount,rate,dueDate,Dates.today(),period=period)
+
 
 end
